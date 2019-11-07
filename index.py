5280e9cee6fa60d8a8243968b332bddb853021fa
Jean-Soh <48852149+Jean-Soh@users.noreply.github.com>
2019-11-07 11:35:03 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,9 +1,20 @@
-import eel
 
-eel.init('web')
+import cv2
 
-@eel.expose
-def click_func_py():
-  eel.changeHtml()
+# [capture]
+capture = cv2.VideoCapture(0)
 
-eel.start('index.html')
+while True:
+    ret, frame = capture.read()
+    if frame is None:
+        break
+    x = 59
+
+    #Code
+
+    # [show]
+    cv2.imshow('Frame', frame)
+
+    keyboard = cv2.waitKey(0)
+    if keyboard == 'q' or keyboard == 27:
+        break
